435dac1cab0a7c590e4bdc37ba4eb1725457cb40
Maaz4324 <maazahmed4324@gmail.com>
2024-02-18 16:34:07 +0530

    working on it

--- tutorial.py
+++ tutorial.py
@@ -5,17 +5,19 @@ pygame.init()
 WIDTH, HEIGHT = 1700, 1000
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulator")
-SPACE = "#ffffff"
+SPACE = "#070F2B"
 YELLOW = '#ed7205'
 BLUE = '#B5C0D0'
 JUPITER = '#d8ca9d'
-SUN_RADIUS = 30
+MARS = '#c1440e'
+SUN_RADIUS = 46
+RADIUS_SCALE = SUN_RADIUS / 696340
 
 
 class Planet:
-    AU = 149.6e6 * 1000
+    AU = 149.6e6 
     G = 6.67428e-11
-    SCALE = 250 / AU # 1AU = 100 pixels
+    SCALE = 100 / AU # 1AU = 100 pixels
     TIMESTAMP = 3600*24 # 1day
 
     def __init__(self, x, y, radius, color, mass):
@@ -43,14 +45,16 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    sun = Planet(0, 0, SUN_RADIUS, YELLOW, 1.98892 * 10**30)
+    sun = Planet(0, 0,SUN_RADIUS , YELLOW, 1.98892 * 10**30)
     sun.sun = True
 
-    earth = Planet(-1* Planet.AU, 0, SUN_RADIUS * 0.0914, BLUE, 5.9722 * 10**24)
+    earth = Planet(-1* Planet.AU, 0, RADIUS_SCALE * 6371, BLUE, 5.9722 * 10**24)
 
-    jupiter = Planet(-1.2* Planet.AU, 0, SUN_RADIUS * 0.1004, JUPITER, 1.89813 * 10**27)
+    mars = Planet(-1.52* Planet.AU, 0, RADIUS_SCALE * 3389.5, MARS, 6.39 * 10**23)
 
-    planets = [sun, earth, jupiter]
+    jupiter = Planet(-5.2* Planet.AU, 0, RADIUS_SCALE * 69911, JUPITER, 1.89813 * 10**27)
+
+    planets = [sun, earth, jupiter, mars]
 
     while run:
         clock.tick(60)
